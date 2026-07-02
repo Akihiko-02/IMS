83bd13a663e2c1b93a9d59665cdad7a2d8d6c384
Nanda Shine <thawn819@gmail.com>
2026-07-02 14:05:41 +0630

    Adding security section

---
+++ src/main/java/com/IMS/InventoryManagementSystem/security/CustomUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.IMS.InventoryManagementSystem.security;
+
+import com.IMS.InventoryManagementSystem.exceptions.NotFoundException;
+import com.IMS.InventoryManagementSystem.models.User;
+import com.IMS.InventoryManagementSystem.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username)
+                .orElseThrow(()-> new NotFoundException("User Email Not Found"));
+        return  AuthUser.builder()
+                .user(user).build();
+    }
+}
